52d7052f43e27a4cc518d562fe4b24c0e25e527e
Oleh S <olehsrh@gmail.com>
2016-10-06 00:53:10 +0300

    Make harvester also supply extensions

--- role.harvester.js
+++ role.harvester.js
@@ -9,9 +9,19 @@ var roleHarvester = {
             }
         }
         else {
-            if (creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns['Spawn1']);
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                        structure.energy < structure.energyCapacity;
+                }
+            });
+            if (targets.length > 0) {
+                if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0]);
+                }
             }
         }
     }
 };
+
+module.exports = roleHarvester;
