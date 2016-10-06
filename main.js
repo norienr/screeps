cef435d6de471be06aa67399d6e6968a7af3c948
Oleh S <olehsrh@gmail.com>
2016-10-06 16:53:08 +0300

    Get Tower to also repair walls

--- main.js
+++ main.js
@@ -1,30 +1,34 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
 
-    for (var name in Memory.creeps) {
-        if (!Game.creeps[name]) {
-            delete Memory.creeps[name];
-            console.log('Clearing non-existing creep memory:', name);
+    var tower = Game.getObjectById('546d7d4873e67f8e030be5f0');
+    if (tower) {
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if (closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if (closestHostile) {
+            tower.attack(closestHostile);
         }
     }
 
-    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-    console.log('Harvesters: ' + harvesters.length);
-
-    if (harvesters.length < 2) {
-        var newName = Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE], undefined, {role: 'harvester'});
-        console.log('Spawning new harvester: ' + newName);
-    }
-
     for (var name in Game.creeps) {
-        var creep = Game.creeps[name];
+        let creep = Game.creeps[name];
         if (creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
         }
         else if (creep.memory.role == 'upgrader') {
             roleUpgrader.run(creep);
+        }
+        else if (creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
         }
     }
 }
