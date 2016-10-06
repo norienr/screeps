e7ee292ac0c687ad90b52676a83989c368980751
Oleh S <olehsrh@gmail.com>
2016-10-06 16:34:48 +0300

    Make main controll lifecycles of harvester creeps

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 var roleHarvester = require('role.harvester');
-var roleBuilder = require('role.builder');
+var roleUpgrader = require('role.upgrader');
 
 module.exports.loop = function () {
 
-    for(var name in Game.creeps) {
+    for (var name in Memory.creeps) {
+        if (!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
+    }
+
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    console.log('Harvesters: ' + harvesters.length);
+
+    if (harvesters.length < 2) {
+        var newName = Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE], undefined, {role: 'harvester'});
+        console.log('Spawning new harvester: ' + newName);
+    }
+
+    for (var name in Game.creeps) {
         var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
+        if (creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
         }
-        if(creep.memory.role == 'builder') {
-            roleBuilder.run(creep);
+        else if (creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
         }
     }
 }
