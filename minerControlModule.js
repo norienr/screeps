defa59b6790c766d6d43391b18e203efb775774e
olehsrh <olehsrh@gmail.com>
2016-11-03 18:12:37 +0200

    Delete spread operator due to no support for some reason

--- minerControlModule.js
+++ minerControlModule.js
@@ -14,9 +14,9 @@ var MODULE = (function (module) {
         const p3 = {x: x + 1, y: y - 1};
         const p4 = {x: x - 1, y: y - 1};
         const filtered = _.filter(posArr, p => p.terrain === 'plain' &&
-        JSON.stringify({...p}) !== JSON.stringify(srcP) && JSON.stringify({...p}) !== JSON.stringify(p1) &&
-        JSON.stringify({...p}) !== JSON.stringify(p2) && JSON.stringify({...p}) !== JSON.stringify(p3) &&
-        JSON.stringify({...p}) !== JSON.stringify(p4));
+        JSON.stringify({x: p.x, y: p.y}) !== JSON.stringify(srcP) && JSON.stringify({x: p.x, y: p.y}) !== JSON.stringify(p1) &&
+        JSON.stringify({x: p.x, y: p.y}) !== JSON.stringify(p2) && JSON.stringify({x: p.x, y: p.y}) !== JSON.stringify(p3) &&
+        JSON.stringify({x: p.x, y: p.y}) !== JSON.stringify(p4));
         let positions = [];
         _.forEach(filtered, f => positions.push(new RoomPosition(f.x, f.y, room.name)));
         if (positions.length) {
