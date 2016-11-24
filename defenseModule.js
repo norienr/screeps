a131fcf4484550c2e6b6eb9410da1dfeb1a8ce82
Vlad Tkachenko <vladyslavtk@gmail.com>
2016-11-24 22:54:28 +0200

    Fix

--- defenseModule.js
+++ defenseModule.js
@@ -28,60 +28,60 @@ const defenseModule = (function () {
             if (typeof closestDamagedStructure !== 'undefined') {
                 if (tower.repair(closestDamagedStructure) === OK) {
                     Memory.lastRepair[tower.id] = Game.time;
+                }
+            }
+        },
+        attackThreats: function (tower, targets) {
+            let threat = targets[0];
+            for (let i in targets) {
+                let numOfTicks1 = targets[i].hits / (this.getPotentialDamage(tower, targets[i]) - this.getPotentialHeal(targets[i], targets));
+                let numOfTicks2 = threat.hits / (this.getPotentialDamage(tower, threat) - this.getPotentialHeal(threat, targets));
+                if (numOfTicks1 > 0 && numOfTicks1 < numOfTicks2) {
+                    threat = targets[i];
+                }
+            }
+            tower.attack(threat);
+        },
+        getPotentialDamage: function (tower, target) {
+            const range = tower.pos.getRangeTo(target.pos);
+            if (range <= 5)
+                return 600;
+            else if (range >= 20)
+                return 150;
+            else
+                return 600 - (range - 5) * 30;
+        },
+        getPotentialHeal: function (target, targets) {
+            const closestHealer = _.filter(target.pos.findInRange(targets, 3),
+                c => c.getActiveBodyparts(HEAL) > 0);
+            let potentialHeal = target.getActiveBodyparts(HEAL) * 12;
+            if (closestHealer.length) {
+                for (let i in closestHealer) {
+                    if (target.pos.getRangeTo(closestHealer[i].pos) == 1)
+                        potentialHeal = +closestHealer[i].getActiveBodyparts(HEAL) * 12;
+                    else if (target.pos.getRangeTo(closestHealer[i].pos) > 1)
+                        potentialHeal = +closestHealer[i].getActiveBodyparts(HEAL) * 4;
+                }
+            }
+            return potentialHeal;
         }
-      }
-    },
-    attackThreats: function (tower, targets) {
-      let threat = targets[0];
-      for (let i in targets) {
-        let numOfTicks1 = targets[i].hits / (this.getPotentialDamage(tower, targets[i]) - this.getPotentialHeal(targets[i], targets));
-        let numOfTicks2 = threat.hits / (this.getPotentialDamage(tower, threat) - this.getPotentialHeal(threat, targets));
-        if (numOfTicks1 > 0 && numOfTicks1 < numOfTicks2) {
-          threat = targets[i];
-        }
-      }
-      tower.attack(threat);
-    },
-    getPotentialDamage: function (tower, target) {
-      const range = tower.pos.getRangeTo(target.pos);
-      if (range <= 5)
-        return 600;
-      else if (range >= 20)
-        return 150;
-      else
-        return 600 - (range - 5) * 30;
-    },
-    getPotentialHeal: function (target, targets) {
-      const closestHealer = _.filter(target.pos.findInRange(targets, 3),
-        c => c.getActiveBodyparts(HEAL) > 0);
-      let potentialHeal = target.getActiveBodyparts(HEAL) * 12;
-      if (closestHealer.length) {
-        for (let i in closestHealer) {
-          if (target.pos.getRangeTo(closestHealer[i].pos) == 1)
-            potentialHeal = +closestHealer[i].getActiveBodyparts(HEAL) * 12;
-          else if (target.pos.getRangeTo(closestHealer[i].pos) > 1)
-            potentialHeal = +closestHealer[i].getActiveBodyparts(HEAL) * 4;
-        }
-      }
-      return potentialHeal;
-    }
-  };
+    };
 
-  const publicAPI = {
-    run: function (roomName) {
+    const publicAPI = {
+        run: function (roomName) {
 
-      if (Memory.lastRepair === undefined) {
-        Memory.lastRepair = {};
-      }
+            if (Memory.lastRepair === undefined) {
+                Memory.lastRepair = {};
+            }
 
-      const targets = _.filter(Game.rooms[roomName].find(FIND_HOSTILE_CREEPS),
-        c => c.owner.username !== 'Source Keeper');
+            const targets = _.filter(Game.rooms[roomName].find(FIND_HOSTILE_CREEPS),
+                c => c.owner.username !== 'Source Keeper');
 
-      if (targets.length) {
-        Game.rooms[roomName].memory.underAttack = true;
-      } else {
-        Game.rooms[roomName].memory.underAttack = false;
-      }
+            if (targets.length) {
+                Game.rooms[roomName].memory.underAttack = true;
+            } else {
+                Game.rooms[roomName].memory.underAttack = false;
+            }
 
             let towers;
             if ((towers = o.getTowers(roomName)).length) {
@@ -92,19 +92,19 @@ const defenseModule = (function () {
                     _.forEach(towers, (tower) => {
                         if (Memory.lastRepair[tower.id] === undefined) {
                             Memory.lastRepair[tower.id] = 0;
+                        }
+                        if (Game.time > (Memory.lastRepair[tower.id] + TOWER_ATTACK_INTERVAL)) {
+                            o.doRepair(tower, o.getClosestDamagedStructs(tower));
+                        }
+                    });
+                }
             }
-            if (Game.time > (Memory.lastRepair[tower.id] + TOWER_ATTACK_INTERVAL)) {
-              o.doRepair(tower, o.getClosestDamagedStructs(tower));
-            }
-          });
+
+
         }
-      }
+    };
 
-
-    }
-  };
-
-  return publicAPI;
+    return publicAPI;
 })();
 
 module.exports = defenseModule;
